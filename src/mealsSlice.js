25e4604d6a2e8eb1ffc8b92a3e6a66a422629da2
dominikaredz <152783687+dominikaredz@users.noreply.github.com>
2025-09-13 20:57:48 +0200

    Update mealsSlice.js

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -1,17 +1,17 @@
-// mealsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
-
 export const mealsSlice = createSlice({
   name: 'meals',
   initialState: [
-   
+    { name: 'Śniadanie', cost: 50, selected: false },
+    { name: 'Podwieczorek', cost: 25, selected: false },
+    { name: 'Obiad', cost: 65, selected: false },
+    { name: 'Kolacja', cost: 70, selected: false },
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload].selected;
     },
   },
 });
-
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
